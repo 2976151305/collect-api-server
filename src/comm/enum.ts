84faeb108661c36014052c3b593eacafdbc627ca
hzh97 <2976151305@qq.com>
2021-04-17 17:07:28 +0800

    update

--- src/comm/enum.ts
+++ src/comm/enum.ts
@@ -17,8 +17,9 @@ export const enum ErrorCode {
   SUCCESS = 200, // 成功
   BAD_REQUEST = 400, // 请求语法错误
   INVALID_TOKEN = 403, // 用户校验错误
+  NOT_FOUND = 404, // 暂无结果
   INTERNAL_SERVER_ERROR = 500, // 服务器内部错误
   INVALID_EMAIL = 1001, // 无效邮箱
   INVALID_PHONE = 1002, // 无效手机号
-  NOT_FOUND = 404
+  REPEAT = 1003 // 数据重复
 }
